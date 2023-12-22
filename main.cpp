7c2dbd546ba36e11ac36c92cf874b1b03bcc34a8
Tomasz <153989224+tkrucz@users.noreply.github.com>
2023-12-22 13:20:14 +0100

    readKeys()
    Still fighting with whereIsPlayer()

--- main.cpp
+++ main.cpp
@@ -16,7 +16,7 @@ extern "C"
 #endif
 
 //TODO main to mainGameLoop()
-int main(int argc, char** argv) {	
+int main(int argc, char** argv) {
 
 	createWindow();
 	createColors();
@@ -33,38 +33,16 @@ int main(int argc, char** argv) {
 		Platform platforms[NUMBER_OF_PLATFORMS];
 		Ladder ladders[NUMBERS_OF_LADDERS];
 		gameInfo.t2 = SDL_GetTicks();
-
 		timeCounting();
-		initializeGameObjects(platforms,ladders);
-		displayWindow(platforms,ladders);
-		refreshWindow();		
+
+		initializeGameObjects(platforms, ladders);
+		displayWindow(platforms, ladders);
+		refreshWindow();
 
 		graivityApply();
-		whereIsPLayer(platforms,ladders);
+		whereIsPLayer(platforms, ladders);
 
-		// TODO 48 -> 69 readKeys() 
-		// obs³uga zdarzeñ (o ile jakieœ zasz³y) / handling of events (if there were any)
-		while (SDL_PollEvent(&SDL.event))
-		{
-			switch (SDL.event.type)
-			{
-			case SDL_KEYDOWN:				
-				if (SDL.event.key.keysym.sym == SDLK_ESCAPE) //if Esc pressed then g.quit=1  so that ending the loop
-					gameInfo.quit = true;
-				else if (SDL.event.key.keysym.sym == SDLK_n)
-					basicSetting();
-				else if (SDL.event.key.keysym.sym == SDLK_p)
-					addPoints();
-				else if (SDL.event.key.keysym.sym == SDLK_l)
-					loseLive();
-				else if (Mario.onPlatform || Mario.onLadder)
-					playerMove();		
-				break;		
-			case SDL_QUIT: //X button in right up corner
-				gameInfo.quit = true;
-				break;
-			}
-		}
+		readKeys();
 	}
 
 	SDLSpace();
